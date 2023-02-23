00a773495597fb80ff26f0959bff21555c22a438
zyedibrahim <zyedibrahim77@gmail.com>
2023-02-23 13:47:20 +0530

    first commit

---
+++ router/movies.router.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import { getmovies, getmoviebyid, postmovies, deletemovies, updatemovies } from './service/movies.service.js';
+const router = express.Router();
+
+
+router.get("/", async function (request, response) {
+    const movies = await getmovies();
+  response.send(movies);
+  });
+  
+  
+  router.get(`/:id`, async function (request, response) {
+    const { id } = request.params;
+   const movies = await getmoviebyid(id);
+    //  const movies = moviesdata.find((mv) => mv.id == id )
+    movies
+      ? response.send(movies)
+      : response.status(404).send({ message: "not found" });
+  
+
+  });
+  
+  router.post("/", async function (request, response) {
+    const data = request.body;
+  
+    const result = await postmovies(data);
+  
+    response.send(result);
+  });
+  
+  
+  
+  router.delete(`/:id`, async function (request, response) {
+    const { id } = request.params;
+  
+    const result =await deletemovies(id);
+    //  const movies = moviesdata.find((mv) => mv.id == id )
+    result.deletedCount > 1
+      ? response.send({Message: "deleted successfully"})
+      : response.status(404).send({ message: "not found" });
+  
+    console.log(result);
+  });
+  router.put(`/:id`, async function (request, response) {
+    const { id } = request.params;
+    const data = request.body
+  
+  
+  
+    const result = await updatemovies(id, data);
+    //  const movies = moviesdata.find((mv) => mv.id == id )
+  
+       response.send(result)
+    
+  
+    console.log(result);
+  });
+
+
+  export default router;
+  
+
---
+++ router/service/movies.service.js
@@ -0,0 +1,33 @@
+import { client } from '../../index.js';
+
+export async function updatemovies(id, data) {
+  return await client
+    .db("b42wd2")
+    .collection("movies")
+    .updateOne({ id: id }, { $set: data });
+}
+export async function deletemovies(id) {
+  return await client
+    .db("b42wd2")
+    .collection("movies")
+    .deleteOne({ id: id });
+}
+export async function postmovies(data) {
+  return await client
+    .db("b42wd2")
+    .collection("movies")
+    .insertMany(data);
+}
+export async function getmoviebyid(id) {
+  return await client
+    .db("b42wd2")
+    .collection("movies")
+    .findOne({ id: id });
+}
+export async function getmovies() {
+  return await client
+    .db("b42wd2")
+    .collection("movies")
+    .find({})
+    .toArray();
+}
